bd432ab4c8e821c13593b58f2a0aea506b6d6f71
saurabhj80 <saurabhj80@gmail.com>
2015-06-03 23:29:13 +0530

    Image should be fine now

--- Silky Socks/TemplateModel.swift
+++ Silky Socks/TemplateModel.swift
@@ -28,14 +28,17 @@ class Template {
     
     // Return the template objects in an array
     class func allTemplates() -> [Template] {
+        
+        typealias Img = UIImage
+        
         return [
-            Template(image: UIImage(named: "blank")!,caption: "", type: .Socks, maskImage: nil),
-            Template(image: UIImage(named: "blackfoot")!,caption: "Black Foot", type: .Socks, maskImage: nil),
-            Template(image: UIImage(named: "kneehigh")!,caption: "Knee High Black Foot", type: .Socks, maskImage: nil),
-            Template(image: UIImage(named: "white_tee")!,caption: "White Tee", type: .Shirt, maskImage: UIImage(named: "white_nocollar")),
-            Template(image: UIImage(named: "tank")!,caption: "Tank", type: .Tank, maskImage: UIImage(named: "tank_nocollar")),
-            Template(image: UIImage(named: "black_sleeve_template")!,caption: "Black Sleeve Tee", type: .Shirt, maskImage: nil),
-            Template(image: UIImage(named: "new_shirt_temp")!, caption: "", type: .Shirt, maskImage: nil)
+            Template(image: Img(named: "blank")!,caption: "", type: .Socks, maskImage: nil),
+            Template(image: Img(named: "blackfootnew")!,caption: "Black Foot", type: .Socks, maskImage: Img(named: "blackfootnew_noblack")),
+            Template(image: Img(named: "kneehigh")!,caption: "Knee High Black Foot", type: .Socks, maskImage: Img(named: "kneehigh_noblack")),
+            Template(image: Img(named: "white_tee")!,caption: "White Tee", type: .Shirt, maskImage: Img(named: "white_nocollar")),
+            Template(image: Img(named: "tank")!,caption: "Tank", type: .Tank, maskImage: Img(named: "tank_nocollar")),
+            Template(image: Img(named: "black_sleeve")!,caption: "Black Sleeve Tee", type: .Shirt, maskImage: Img(named: "black_sleeve_nocollar")),
+            Template(image: Img(named: "new_shirt_temp")!, caption: "", type: .Shirt, maskImage: nil)
         ]
     }
 }
--- Silky Socks/Utilities.swift
+++ Silky Socks/Utilities.swift
@@ -45,29 +45,13 @@ extension UIColor {
         
         var array = [UIColor]()
         
-        for var i = 0; i < 6; ++i {
-            array.append(UIColor.getColor(red: i*51, green: i*51, blue: i*51, alpha: 1))
-        }
-        
-        for var i = 0; i < 7; ++i {
-            array.append(UIColor.getColor(red: 255, green: i*34, blue: 0, alpha: 1))
-        }
-        
-        for var i = 7; i >= 0; --i {
-            array.append(UIColor.getColor(red: 34*i, green: 255, blue: 0, alpha: 1))
-        }
-        
-        for var i = 1; i < 8; ++i {
-            array.append(UIColor.getColor(red: 0, green: 255, blue: i*34, alpha: 1))
-        }
-        
-        for var i = 7; i >= 0; --i {
-            array.append(UIColor.getColor(red: 0, green: i*34, blue: 255, alpha: 1))
-        }
-        
-        for var i = 1; i < 7; ++i {
-            array.append(UIColor.getColor(red: i*34, green: 0, blue: 255, alpha: 1))
-        }
+        for var i = 0; i < 6 ; ++i   { array.append(UIColor.getColor(red: i*51 , green: i*51  , blue: i*51 , alpha: 1)) }
+        for var i = 0; i < 7 ; ++i   { array.append(UIColor.getColor(red: 255  , green: i*34  , blue: 0    , alpha: 1)) }
+        for var i = 7; i >= 0; --i   { array.append(UIColor.getColor(red: 34*i , green: 255   , blue: 0    , alpha: 1)) }
+        for var i = 1; i < 8 ; ++i   { array.append(UIColor.getColor(red: 0    , green: 255   , blue: i*34 , alpha: 1)) }
+        for var i = 7; i >= 0; --i   { array.append(UIColor.getColor(red: 0    , green: i*34  , blue: 255  , alpha: 1)) }
+        for var i = 1; i < 7 ; ++i   { array.append(UIColor.getColor(red: i*34 , green: 0     , blue: 255  , alpha: 1)) }
+        for var i = 7; i >= 0; --i   { array.append(UIColor.getColor(red: 255  , green: 0     , blue: i*34 , alpha: 1)) }
         
         return array
     }
